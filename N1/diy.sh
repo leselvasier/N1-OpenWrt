ecac2a2ed93e0f73faa72983376c10fc6f69c125
leselvasier <leselvasier@gmail.com>
2025-06-16 09:43:06 +0800

    N1 OpenWrt

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Default IP
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
